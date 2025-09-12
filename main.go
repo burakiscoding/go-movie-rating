6ec02432d9c27dd612711b389c9b0aa93dd60de3
burakiscoding <burak.kurtarir35@gmail.com>
2025-09-12 21:44:01 +0300

    structuring project

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
-	"github.com/burakiscoding/go-movie-rating/helpers"
+	"github.com/burakiscoding/go-movie-rating/middleware"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	helpers.SayHello()
+	router := http.NewServeMux()
+	router.HandleFunc("GET /movies", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "List of movies")
+	})
+	router.HandleFunc("GET /movies/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		io.WriteString(w, "Movie "+id)
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: middleware.Logging(router),
+	}
+
+	fmt.Println("Server listening on port 8080")
+	server.ListenAndServe()
 }

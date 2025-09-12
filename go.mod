33e8b28a61e75ed20f6ac18861f8e1afed698c63
burakiscoding <burak.kurtarir35@gmail.com>
2025-09-12 19:07:32 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/burakiscoding/go-movie-rating
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/burakiscoding/go-movie-rating/helpers"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	helpers.SayHello()
+}
